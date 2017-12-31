84a41fb0e0462ac83151096c88685dc6f32204b5
Andor Saga <andor@idearebel.com>
2017-12-31 14:01:50 -0500

    finished ep.1

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,32 @@
-function loadImage(url) {
-    const p = new Promise(function(resolve) {
-        const image = new Image();
+import SpriteSheet from './SpriteSheet.js';
+import { loadImage, loadLevel } from './loaders.js';
 
-        image.addEventListener('load', () => {
-            resolve(image);
-        });
-        image.src = url;
+function drawBackground(background, context, sprites) {
+
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        }
     });
-    return p;
 }
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
-context.fillRect(0, 0, 64, 64);
 
 loadImage('/image/tiles.png')
     .then(function(image) {
-        context.drawImage(image, 0, 0, 16, 16, 0, 0, 32, 32);
+
+        const sprites = new SpriteSheet(image, 16, 16);
+        sprites.define('ground', 0, 0);
+        sprites.define('sky', 3, 23);
+
+        loadLevel('1-1')
+            .then(level => {
+                level.backgrounds.forEach(b => {
+                    drawBackground(b, context, sprites);
+                });
+            });
     });
