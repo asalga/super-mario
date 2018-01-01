91176224bd250bfcb12a3554e0382c275b5493f1
Andor Saga <andor@idearebel.com>
2018-01-01 17:30:45 -0500

    finished ep.2

--- public/js/main.js
+++ public/js/main.js
@@ -1,21 +1,16 @@
+import Compositor from './Compositor.js';
 import { loadLevel } from './loaders.js';
 import { loadMarioSprite, loadBackgroundSprites } from './Sprites.js';
+import createBackgroundLayer from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function drawBackground(background, context, sprites) {
-
-    background.ranges.forEach(([x1, x2, y1, y2]) => {
-
-        for (let x = x1; x < x2; ++x) {
-            for (let y = y1; y < y2; ++y) {
-                sprites.drawTile(background.tile, context, x, y);
-            }
-        }
-    });
+function createSpriteLayer(sprites, pos) {
+    return function drawSpriteLayer(context) {
+        sprites.draw('idle', context, pos.x, pos.y);
+    }
 }
-
 
 Promise
     .all([
@@ -23,10 +18,27 @@ Promise
         loadBackgroundSprites(),
         loadLevel('1-1')
     ])
-    .then(([marioSprite, sprites, level]) => {
+    .then(([marioSprite, backgroundSprites, level]) => {
 
-        level.backgrounds.forEach(b => {
-            drawBackground(b, context, sprites);
-        });
-        marioSprite.draw('idle', context, 64, 64);
+        const comp = new Compositor();
+        const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
+        comp.layers.push(backgroundLayer);
+
+        const pos = {
+            x: 64,
+            y: 64
+        };
+
+        const spriteLayer = createSpriteLayer(marioSprite, pos);
+        comp.layers.push(spriteLayer);
+
+        function update() {
+            comp.draw(context);
+
+            marioSprite.draw('idle', context, pos.x, pos.y);
+            pos.x += 2;
+            pos.y += 2;
+            requestAnimationFrame(update);
+        }
+        update();
     });
