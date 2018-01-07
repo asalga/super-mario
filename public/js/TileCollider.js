63989b18f6cd8bc6f4fa3581dc654caabaeed1ac
Andor Saga <andor.saga@gmail.com>
2018-01-06 23:00:40 -0500

    fixing collision box.

--- public/js/TileCollider.js
+++ public/js/TileCollider.js
@@ -6,28 +6,30 @@ export default class TileCollider {
     }
 
     checkY(entity) {
-        const match = this.resolver.matchByPosition(entity.pos.x, entity.pos.y);
-        if (!match) {
-            return;
-        }
+        // const match = this.resolver.searchByPosition(entity.pos.x, entity.pos.y);
+        const matches = this.resolver.searchByRange(
+            entity.pos.x, entity.pos.x + entity.size.x,
+            entity.pos.y, entity.pos.y + entity.size.y);
 
-        if (match.tile.name !== 'ground') {
-            return;
-        }
+        matches.forEach(m => {
 
-        if (entity.vel.y > 0) {
-            if (entity.pos.y > match.y1) {
-                entity.pos.y = match.y1;
-                entity.vel.y = 0;
+            if (m.tile.name !== 'ground') {
+                return;
             }
-        }
-        else if (entity.vel.y < 0) {
-            // console.log(entity.pos.y, match.y2);
-            if (entity.pos.y < match.y2) {
-                entity.pos.y = match.y2;
-                entity.vel.y = 0;
+
+            if (entity.vel.y > 0) {
+                if (entity.pos.y + entity.size.y > m.y1) {
+                    entity.pos.y = m.y1 - entity.size.y;
+                    entity.vel.y = 0;
+                }
+            } else if (entity.vel.y < 0) {
+                // console.log(entity.pos.y, m.y2);
+                if (entity.pos.y < m.y2) {
+                    entity.pos.y = m.y2;
+                    entity.vel.y = 0;
+                }
             }
-        }
+        });
     }
 
     test(entity) {
--- public/js/TileResolver.js
+++ public/js/TileResolver.js
@@ -9,6 +9,21 @@ export default class TileResolver {
         return Math.floor(pos / this.tileSize);
     }
 
+    toIndexRange(pos1, pos2) {
+        const pMax = Math.ceil(pos2 / this.tileSize) * this.tileSize;
+
+        // collect all the indices in the range
+        const range = [];
+        let pos = pos1;
+
+        do {
+            range.push(this.toIndex(pos));
+            pos += this.tileSize;
+        } while (pos < pMax);
+
+        return range;
+    }
+
     // return some metadata about a tile in the level
     getByIndex(indexX, indexY) {
 
@@ -28,7 +43,24 @@ export default class TileResolver {
     }
 
     // returns tile metadata based on position
-    matchByPosition(posX, posY) {
-        return this.getByIndex(this.toIndex(posX), this.toIndex(posY));
+    searchByPosition(posX, posY) {
+        // 0, 5
+        const indexX = this.toIndex(posX);
+        const indexY = this.toIndex(posY);
+        return this.getByIndex(indexX, indexY);
+    }
+
+    searchByRange(x1, x2, y1, y2) {
+        const matches = [];
+        this.toIndexRange(x1, x2).forEach(indexX => {
+            this.toIndexRange(y1, y2).forEach(indexY => {
+                const match = this.getByIndex(indexX, indexY);
+                if (match) {
+                    matches.push(match);
+                }
+            })
+        });
+
+        return matches;
     }
 }
